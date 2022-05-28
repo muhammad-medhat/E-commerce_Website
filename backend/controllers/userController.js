988cd0fb1c0bb9f5becef0ae29260b690bb34046
Hayah Hazem <hayahhazem1999@gmail.com>
2022-05-28 21:27:25 +0200

    registration function and the token authorization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,9 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
 const User = require("../model/userModel");
+
 
 const updateUser = asyncHandler(async (req, res) => {
   const id = req.params.id;
@@ -53,4 +56,66 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { updateUser };
+// Register User with POST route
+const regUser = asyncHandler(async (req , res)=>{
+  const{username,password,email} = req.body;
+
+  if(!username ||!password ||!email){
+    res.status(400);
+   throw new Error('Please add all fields');
+  }
+  // Check if user exists
+  const userExists=await User.findOne({email});
+  
+  if(userExists){
+    res.status(400);
+    throw new Error('User already exists');
+  }
+// Encrypting password
+const salt=await bcrypt.genSalt(10)
+const hashedPassword=await bcrypt.hash(password,salt)
+
+const user = await User.create({
+  username,
+  password:hashedPassword,
+  email
+})
+
+if(user){
+  res.status(201).json({
+    _id:user.id,
+    username:user.username,
+    email:user.email,
+    token: generateToken(user._id)
+ })
+}else{
+  res.status(400)
+  throw new Error('Invalid user data')
+}
+});
+
+
+ // GET a user
+
+ const getUser = asyncHandler(async (req, res) => {
+  const { _id, username, email} = await User.findById(req.user.id);
+   res.status(200).json({
+     id: _id,
+     username,
+     email
+   })
+ });
+
+
+// Generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({id},process.env.TOKEN_SECRET,{
+    expiresIn:'30d',
+  })
+}; 
+
+
+module.exports = { updateUser,
+  regUser,
+  getUser
+ };
