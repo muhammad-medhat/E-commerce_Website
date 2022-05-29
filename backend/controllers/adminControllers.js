7f6bf9574af162875460d87cac03a646265b5ba2
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-05-29 11:39:09 +0200

    feat: Allow the admin to view all users and update user passwords

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
 const Admin = require("../model/adminModel");
+const User = require("../model/userModel");
 
 // Admin log in
 
@@ -14,17 +15,15 @@ const loginAdmin = asyncHandler(async (req, res) => {
   if (admin && (await bcrypt.compare(password, admin.password))) {
     const token = generateToken(admin._id);
     res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: maxAge * 1000,
-      });
+      httpOnly: true,
+      maxAge: maxAge * 1000,
+    });
     res.json({
       _id: admin.id,
       username: admin.username,
       email: admin.email,
       token: generateToken(admin._id),
     });
-    
-    
   } else {
     res.status(400);
     throw new Error("Invalid admin email or password");
@@ -33,8 +32,8 @@ const loginAdmin = asyncHandler(async (req, res) => {
 
 // logout the admin
 const logoutAdmin = asyncHandler(async (req, res) => {
-    res.cookie('jwt', '', { maxAge: 1 });
-    res.redirect('/')
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
 });
 
 // Generate JWT token
@@ -45,4 +44,26 @@ const generateToken = (id) => {
   });
 };
 
-module.exports = { loginAdmin, logoutAdmin };
+// GET all Users
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+  res.status(200).json({ users });
+});
+
+// POST new user password
+const updateUserPassword = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid user email");
+  } else {
+    // Encrypting password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await User.findByIdAndUpdate(user._id, { password: hashedPassword });
+    res.status(200).json({ message: "Password updated successfully" });
+  }
+});
+
+module.exports = { loginAdmin, logoutAdmin, getAllUsers, updateUserPassword };
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {
- loginAdmin, logoutAdmin
+  loginAdmin,
+  logoutAdmin,
+  getAllUsers,
+  updateUserPassword,
 } = require("../controllers/adminControllers");
 //const { verifyAuth } = require("../middleware/authMiddleware");
 
 router.post("/login", loginAdmin);
 router.get("/logout", logoutAdmin);
+router.get("/users/all", getAllUsers);
+router.post("/users/password", updateUserPassword);
 module.exports = router;
