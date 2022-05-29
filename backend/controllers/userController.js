1ea6e97b9677ab81d488738e9b5a576f501a79de
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-05-29 11:38:05 +0200

    fix: Hash the user password when it is updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,8 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
 const User = require("../model/userModel");
-
 
 const updateUser = asyncHandler(async (req, res) => {
   const id = req.params.id;
@@ -40,11 +39,14 @@ const updateUser = asyncHandler(async (req, res) => {
       throw new Error("Wrong password");
     }
   } else if (newPassword) {
+    // Encrypting password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
     if (user.password === password) {
       let updatedUser = await User.findByIdAndUpdate(
         id,
         {
-          password: newPassword,
+          password: hashedPassword,
         },
         { new: true }
       );
@@ -57,78 +59,72 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 // Register User with POST route
-const regUser = asyncHandler(async (req , res)=>{
-  const{username,password,email} = req.body;
+const regUser = asyncHandler(async (req, res) => {
+  const { username, password, email } = req.body;
 
-  if(!username ||!password ||!email){
+  if (!username || !password || !email) {
     res.status(400);
-   throw new Error('Please add all fields');
+    throw new Error("Please add all fields");
   }
   // Check if user exists
-  const userExists=await User.findOne({email});
-  
-  if(userExists){
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
     res.status(400);
-    throw new Error('User already exists');
+    throw new Error("User already exists");
   }
-// Encrypting password
-const salt=await bcrypt.genSalt(10)
-const hashedPassword=await bcrypt.hash(password,salt)
+  // Encrypting password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-const user = await User.create({
-  username,
-  password:hashedPassword,
-  email
-})
+  const user = await User.create({
+    username,
+    password: hashedPassword,
+    email,
+  });
 
-if(user){
-  const token = generateToken(user._id);
+  if (user) {
+    const token = generateToken(user._id);
     res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: maxAge * 1000,
-      });
-  res.status(201).json({
-    _id:user.id,
-    username:user.username,
-    email:user.email,
-    token: generateToken(user._id)
- })
-}else{
-  res.status(400)
-  throw new Error('Invalid user data')
-}
+      httpOnly: true,
+      maxAge: maxAge * 1000,
+    });
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
+// GET a user
 
- // GET a user
+const getUser = asyncHandler(async (req, res) => {
+  const { _id, username, email } = await User.findById(req.user.id);
+  res.status(200).json({
+    id: _id,
+    username,
+    email,
+  });
+});
 
- const getUser = asyncHandler(async (req, res) => {
-  const { _id, username, email} = await User.findById(req.user.id);
-   res.status(200).json({
-     id: _id,
-     username,
-     email
-   })
- });
-
-// log out users 
+// log out users
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1 });
-  res.redirect('/')
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
 });
 
 // Generate JWT token
 const maxAge = 3 * 24 * 60 * 60;
 const generateToken = (id) => {
-  return jwt.sign({id},process.env.TOKEN_SECRET,{
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
     expiresIn: maxAge,
-  })
-}; 
+  });
+};
 
-
-module.exports = { updateUser,
-  regUser,
-  getUser, 
-  logoutUser
- };
+module.exports = { updateUser, regUser, getUser, logoutUser };
