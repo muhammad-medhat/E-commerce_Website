aead9d0591c7c112c12daf854261bd46d569516d
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-05-29 20:11:34 +0200

    feat: Add a status and timestamps to the user schema

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  username: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
-});
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    status: {
+      type: String,
+      enum: ["ACTIVE", "DEACTIVATED", "SUSPENDED"],
+      require: true,
+      default: "ACTIVE",
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
