5e8cd9f2ed12fae3213970a9a23d1679e1fb25ad
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-06-01 14:52:10 +0200

    refactor: Change the quantity error message

--- backend/controllers/cartContoller.js
+++ backend/controllers/cartContoller.js
@@ -38,7 +38,7 @@ const addItemToCart = asyncHandler(async (req, res) => {
   ) {
     res.status(400);
     throw new Error(
-      "The quantity requested is less than the quantity in stock"
+      "The quantity requested is greater than the quantity in stock"
     );
   }
 
