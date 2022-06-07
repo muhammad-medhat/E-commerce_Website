9980981403997ec5e50a009b8852f4b7ebc5efd2
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-06-07 20:12:21 +0200

    fix: Change the delivery field in the product model to daysTillDelivery

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -17,10 +17,10 @@ const productSchema = mongoose.Schema({
     ref: "Brand",
   },
   quantityInStock: { type: Number, require: true },
-  deliveryTime: { type: Date, require: true },  
-  
-  stock:{type:Number, default:0},
-  handling: { type: Number, default: 1},
+  daysTillDelivery: { type: Number, require: true },
+
+  stock: { type: Number, default: 0 },
+  handling: { type: Number, default: 1 },
 });
 
 module.exports = mongoose.model("Product", productSchema);
