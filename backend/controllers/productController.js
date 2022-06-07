5f0164f4573df5326104e93c84f1bdc41d438d61
Muhammad <Muhammad.medhat@gmail.com>
2022-06-07 15:08:24 +0200

    fix: check for product before insert

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 const fetch = (...args) =>
-import('node-fetch').then(({ default: fetch }) => fetch(...args));
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -47,36 +47,32 @@ const getProduct = asyncHandler(async (req, res) => {
  * */
 const deleteProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
-  Product.findByIdAndUpdate(id, { quantityInStock: 0 }, { new: true }, (err, product) => {
-    if (err) {
-      res.status(400).json({
-        message: "Product not found",
+  Product.findByIdAndUpdate(
+    id,
+    { quantityInStock: 0 },
+    { new: true },
+    (err, product) => {
+      if (err) {
+        res.status(400).json({
+          message: "Product not found",
+        });
+      }
+      res.status(200).json({
+        message: "Product deleted",
+        product,
       });
     }
-    res.status(200).json({
-      message: "Product deleted",
-      product,
-    });
-  });
+  );
 });
-      
 
 /**
  * @desc    Create product
- * @route   POST /api/products/:id
+ * @route   POST /api/products/
  * @access  Private Admin
  */
 const createProduct = asyncHandler(async (req, res) => {
   // const product = req.body;
-  const { name, description, images, mainImage, price, category, brand, quantityInStock, stock, handling } = req.body;
-
-  const productCheck = Product.find({ name });
-  if (productCheck) {
-    res.status(400).json({
-      message: "Product already exists",
-    });
-  } else {
-  const product = await Product.create({
+  const {
     name,
     description,
     images,
@@ -87,12 +83,32 @@ const createProduct = asyncHandler(async (req, res) => {
     quantityInStock,
     stock,
     handling,
-  } );
-  res.json({
-    code: res.statusCode,
-    message: "Product created",
-    product,
-  });
+  } = req.body;
+
+  //check if product exists
+  const product = await Product.findOne({ name });
+  if (product) {
+    res.status(400).json({
+      message: "Product already exists",
+    });
+  } else {
+    const newProduct = new Product({
+      name,
+      description,
+      images,
+      mainImage,
+      price,
+      category,
+      brand,
+      quantityInStock,
+      stock,
+      handling,
+    });
+    await newProduct.save();
+    res.status(201).json({
+      message: "Product created",
+      newProduct,
+    });
   }
 });
 
@@ -134,47 +150,44 @@ const updateProduct = asyncHandler(async (req, res) => {
  */
 const generateProducts = asyncHandler(async (req, res) => {
   // res.json({ message: "generating products" });
-  const products =[]
-  fetch('https://dummyjson.com/products')
-  .then(res => res.json())
-  .then(data => {
-    Array.from(data.products).forEach( async element => {
-      const product = await ProductM.create({
-        name: element.title,
-        description: element.description,
-        images: element.images,
-        thumbnail: element.thumbnail,
-        price: element.price,
-        category: catController.getCatByName(element.category)._id,
-        //brand: element.brand,
-        quantity: element.quantity,
+  const products = [];
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      Array.from(data.products).forEach(async (element) => {
+        const product = await ProductM.create({
+          name: element.title,
+          description: element.description,
+          images: element.images,
+          thumbnail: element.thumbnail,
+          price: element.price,
+          category: catController.getCatByName(element.category)._id,
+          //brand: element.brand,
+          quantity: element.quantity,
+        });
+        products.push(product);
       });
-      products.push(product);
+      res.json({
+        message: "products generated",
+        products,
+        data,
+      });
     });
-    res.json({ 
-      message: "products generated",
-      products,
-      data
-     });
-  });
 });
-
 
 const genCats = asyncHandler(async (req, res) => {
   //res.json({ message: "generating categories" });
 
-  fetch('https://dummyjson.com/products/categories')
-  .then(res => res.json())
-  .then(dt=>{
-    dt.forEach(async (item) => {
-      const category = await Category.create({
-        name: item,
+  fetch("https://dummyjson.com/products/categories")
+    .then((res) => res.json())
+    .then((dt) => {
+      dt.forEach(async (item) => {
+        const category = await Category.create({
+          name: item,
+        });
       });
+      res.json({ dt });
     });
-    res.json({dt})
-  });
-    
-
 });
 const getCats = asyncHandler(async (req, res) => {
   const categories = await Category.find();
@@ -194,5 +207,6 @@ module.exports = {
   getCats,
   getBrands,
 
-  generateProducts, genCats,
+  generateProducts,
+  genCats,
 };
