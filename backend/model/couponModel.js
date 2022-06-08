969752c150a262f398ec05f2977488aef31eb2e6
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-06-08 18:28:42 +0200

    feat: Allow users to use the coupon only once

--- backend/model/couponModel.js
+++ backend/model/couponModel.js
@@ -5,6 +5,14 @@ const couponSchema = mongoose.Schema({
   description: { type: String },
   expiresAt: { type: Date },
   discount: { type: Number },
+  usersUsed: [
+    {
+      type: Set,
+      of: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: new Set(),
+    },
+  ],
 });
 
 module.exports = mongoose.model("Coupon", couponSchema);
