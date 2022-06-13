770d6d694d8cb3cb8bae8b757dac98c343b87619
Dahlia Ayman <dahliaayman5@gmail.com>
2022-06-13 19:13:15 +0200

    Add: landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 import { Navbar, Footer } from './components';
-import { UserInfo, Login } from './pages';
+import { UserInfo, Login ,Home } from './pages';
 
-//import Home from'./pages/home/Home'
 
 const App = () => {
     return (
@@ -14,6 +13,7 @@ const App = () => {
             <Routes>
                 <Route path='/profile' element={<UserInfo />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/'  element={<Home/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
