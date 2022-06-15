0838123a6d2ea57b5fed9928016e2bce7b827f24
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-06-15 20:20:36 +0200

    fix: throw and error if the order does not exist

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -41,6 +41,10 @@ const getAllOrders = asyncHandler(async (req, res) => {
 const getOrder = asyncHandler(async (req, res) => {
   if (exists(req.params.id)) {
     const order = await Order.findById(req.params.id);
+    if (!order) {
+      res.status(400);
+      throw new Error("Order not found");
+    }
     res.status(200).json({
       order,
     });
