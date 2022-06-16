a49fe4e14e91a83f28963abe07c895d0dfcfa2ae
Hayah Hazem <hayahhazem1999@gmail.com>
2022-06-16 22:16:05 +0200

    Fix coupon logic

--- backend/controllers/couponController.js
+++ backend/controllers/couponController.js
@@ -24,10 +24,15 @@ const createCoupon = asyncHandler(async (req, res) => {
     throw new Error("coupon already exists");
   }
   const coupon = await stripe.coupons.create({
+    id: code,
     percent_off: discount,
     duration: "once",
     max_redemptions,
   });
+  await stripe.promotionCodes.create({
+    coupon: coupon.id,
+  });
+  
   const createdCoupon = await Coupon.create({
     code,
     description,
@@ -47,8 +52,29 @@ const createCoupon = asyncHandler(async (req, res) => {
 // @access  Private
 
 const viewCoupon = asyncHandler(async (req, res) => {
+  const coupon = await stripe.coupons.list();  
   const coupons = await Coupon.find();
-  res.status(200).json({ coupons });
+  res.status(200).json({ coupons, coupon });
 });
 
-module.exports = { createCoupon, viewCoupon };
+// @desc    delete a Coupons
+// @route   GET /api/coupons/
+// @access  Private
+
+const delCoupon = asyncHandler(async (req, res) => {
+  const coupon = await Coupon.findOne({ code: req.body.code });
+  if (!coupon) {
+    res.status(400);
+    throw new Error("coupon not found");
+  }
+  await Coupon.findByIdAndDelete(coupon._id);
+  const deleted = await stripe.coupons.del(
+    req.body.code  
+  );
+  res.status(201).json({
+    id: coupon._id,
+    deleted,
+  });
+  
+});
+module.exports = { createCoupon, viewCoupon, delCoupon };
